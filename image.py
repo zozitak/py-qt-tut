1011d13c9e558f54d31a8a8d849329c04ceb0d6f
Takács Zoltán Gábor <zozitak@gmail.com>
2021-11-27 01:27:33 +0100

    Add new File

---
+++ image.py
@@ -0,0 +1,24 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout, QWidget
+from PyQt5.QtGui import QPixmap
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel()
+        self.label.setPixmap(QPixmap("./cloud.jpg"))
+
+        self.grid = QGridLayout()
+        self.grid.addWidget(self.label,1,1)
+        self.setLayout(self.grid)
+
+        self.setGeometry(50,50,320,200)
+        self.setWindowTitle("PyQT show image")
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
