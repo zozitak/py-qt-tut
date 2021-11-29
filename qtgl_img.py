f346644058185c5245619e93ef598b1f869809fc
Takács Zoltán Gábor <zozitak@gmail.com>
2021-11-29 21:59:42 +0100

    OpenGl
    opengl magic is currently uploading to zoli.brain, please wait patiently

--- qtgl_img.py
+++ qtgl_img.py
@@ -1,9 +1,11 @@
 import sys
-from OpenGL.GL import *
-from OpenGL.GLU import *
+#from OpenGL.GL import *
+#from OpenGL.GLU import *
 from PyQt5 import QtGui
 from PyQt5.QtOpenGL import *
 from PyQt5 import QtCore, QtWidgets, QtOpenGL
+from PIL import Image
+import numpy as np
 
 
 class Ui_MainWindow(QtWidgets.QWidget):
@@ -17,9 +19,17 @@ class Ui_MainWindow(QtWidgets.QWidget):
         self.setLayout(mainLayout)
 
 class glWidget(QGLWidget):
+    texture_id = 0
+
     def __init__(self, parent=None):
         QGLWidget.__init__(self, parent)
         self.setMinimumSize(640, 640)
+        global texture_id
+        global img
+        global img_data 
+        texture_id = glGenTextures(1)
+        img = Image.open('resources/wood.jpg')
+        img_data = np.array(list(img.getdata()), np.int8)
 
     def paintGL(self):
         glClearColor(0.5, 0.2, 1.0, 1.0)
@@ -27,6 +37,14 @@ class glWidget(QGLWidget):
         glLoadIdentity()
         glTranslatef(0.0, 0.0, 0.0)
         glPolygonMode(GL_FRONT, GL_FILL)
+
+        
+        glBindTexture(GL_TEXTURE_2D, texture_id)
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img.size[0], img.size[1], 0, GL_RGBA, GL_UNSIGNED_BYTE, img_data)
+        
+        glEnable(GL_TEXTURE_2D)
+
+        #draw rect 
         x1 = -5.0
         y1 = -5.0
         z1 = -0.1 
@@ -38,6 +56,7 @@ class glWidget(QGLWidget):
         glVertex3f(width, y1, z1)
         glVertex3f(width, height, z1)
         glVertex3f(x1, height, z1)
+        
         glEnd()
         glFlush()
 
@@ -52,20 +71,21 @@ class glWidget(QGLWidget):
         gluPerspective(45.0,1.0,0.1, 100.0) 
         glMatrixMode(GL_MODELVIEW)
 
-def read_texture(filename):
-    img = Image.open(filename)
-    img_data = numpy.array(list(img.getdata()), numpy.int8)
-    textID = glGenTextures(1)
-    glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
-    return textID
+# def read_texture(filename):
+#     img = Image.open(filename)
+#     img_data = np.array(list(img.getdata()), np.int8)
+#     texture_id = glGenTextures(1)
+#     glBindTexture(GL_TEXTURE_2D,texture_id) 
+#     glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
+#     glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
+#     glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL)
+#     #glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img.size[0], img.size[1], 0, GL_RGB, GL_UNSIGNED_BYTE, img_data)
+#     return texture_id
 
 if __name__ == '__main__':    
     app = QtWidgets.QApplication(sys.argv)    
